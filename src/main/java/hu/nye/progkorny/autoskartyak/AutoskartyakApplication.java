1ce161699c6d120024c8155d35492091f69559ad
Hódi Szilárd <hodiszilard89@gmail.com>
2022-06-03 08:23:17 +0200

    alap projekt, amit a spring init. készített feltöltése a main-re

---
+++ src/main/java/hu/nye/progkorny/autoskartyak/AutoskartyakApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkorny.autoskartyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoskartyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoskartyakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/autoskartyak/autoskartyak/AutoskartyakApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.autoskartyak.autoskartyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoskartyakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
